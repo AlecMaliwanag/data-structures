c9d9be2d19e4d093db86d7c577a46f63973c9fb6
AlecMaliwanag <amaliwanag@gmail.com>
2016-09-15 10:14:24 -0700

    tree completed

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,9 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,9 +13,25 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var tree = Tree(value);
+  this.children.push(tree);
 };
 
 treeMethods.contains = function(target) {
+  var found = false;
+  var recurse = function(node, target) { 
+    if (node.value === target) {
+      found = true;
+    } else {
+      if (node.children.length > 0) {
+        node.children.forEach(function(child) {
+          return recurse(child, target); 
+        });
+      } 
+    }
+  };
+  recurse(this, target);
+  return found;
 };
 
 
