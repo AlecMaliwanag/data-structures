1df4f49a306d6e026ace096970dc502e2762f4b9
AlecMaliwanag <amaliwanag@gmail.com>
2016-09-16 18:42:34 -0700

    Implement hashTable resizing

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -34,27 +34,54 @@ HashTable.prototype.insert = function(k, v) {
   }
 
   if ( this.counter > Math.floor((this._limit * .75))) {
-    this.counter = 0;
-    var tempStorage = [];
+    this.doubledown();
 
-    for (var i = 0; i < this._limit; i++) {
-      //console.log(i, this._storage.get(i));
-      if (this._storage.get(i) !== undefined) {
-        tempStorage.push(this._storage.get(i));
-        this._storage.set(i, undefined);
-      }
-    }
-    
-    tempStorage = _.flatten(tempStorage, true);
-    this._limit = this._limit * 2;
-    console.log(this._limit);
-    this._storage = LimitedArray(this._limit);
-    for (var i = 0; i < tempStorage.length; i ++) {
-      this.insert(tempStorage[i][0], tempStorage[i][1]);
-    }
   }
 
 };
+
+HashTable.prototype.doubledown = function() {
+  this.counter = 0;
+  var tempStorage = [];
+
+  for (var i = 0; i < this._limit; i++) {
+    //console.log(i, this._storage.get(i));
+    if (this._storage.get(i) !== undefined) {
+      tempStorage.push(this._storage.get(i));
+      this._storage.set(i, undefined);
+    }
+  }
+
+  tempStorage = _.flatten(tempStorage, true);
+  this._limit = this._limit * 2;
+  console.log(this._limit);
+  this._storage = LimitedArray(this._limit);
+  for (var i = 0; i < tempStorage.length; i ++) {
+    this.insert(tempStorage[i][0], tempStorage[i][1]);
+  }
+};
+
+HashTable.prototype.tummytuck = function() {
+  this.counter = 0;
+  var tempStorage = [];
+
+  for (var i = 0; i < this._limit; i++) {
+    //console.log(i, this._storage.get(i));
+    if (this._storage.get(i) !== undefined) {
+      tempStorage.push(this._storage.get(i));
+      this._storage.set(i, undefined);
+    }
+  }
+
+  tempStorage = _.flatten(tempStorage, true);
+  this._limit = Math.floor(this._limit / 2);
+  console.log(this._limit);
+  this._storage = LimitedArray(this._limit);
+  for (var i = 0; i < tempStorage.length; i ++) {
+    this.insert(tempStorage[i][0], tempStorage[i][1]);
+  }
+};
+
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
@@ -72,8 +99,22 @@ HashTable.prototype.retrieve = function(k) {
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, undefined);
+  // this._storage.set(index, undefined);
   //this deletes the whole bucket
+  var temp = this._storage.get(index);
+  if (temp !== undefined) {
+    for (var i = 0; i < temp.length; i++) {
+      //console.log(temp[i]);
+      if (temp[i][0] === k) {
+        temp.splice(i, 1);
+        this.counter--;
+      } 
+    }
+  }
+  if ( this.counter < Math.floor((this._limit * .25))) {
+    this.tummytuck();
+
+  }
 };
 
 
